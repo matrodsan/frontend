4b251f40dcff6909357f709549f242d7ccec851d
Mateus Rodrigues Santos <mateusrodriguessantos@hotmail.com>
2023-06-30 17:37:36 -0300

    Tarefa concluída e pronta para entrega.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,20 +1,30 @@
+import styles from "./About.module.css";
+
 const About = () => {
   return (
-    <div>
+    <div className={styles.about}>
+      <img src="/logo192.png" alt="React logo" />
       <h1>Sobre nós</h1>
       <p>
-        Bem-vindo ao nosso blog! Aqui no nosso espaço virtual, você encontrará um universo de informações, inspirações e
-        histórias fascinantes. Nosso blog é um lugar onde compartilhamos ideias, conhecimentos e experiências sobre uma
-        ampla variedade de temas. Navegue por nossas páginas e descubra artigos e postagens emocionantes sobre
-        tecnologia. Queremos ser uma fonte confiável e enriquecedora de conteúdo para todos os nossos leitores. Queremos
-        cativar sua imaginação, instigar sua curiosidade e oferecer perspectivas únicas sobre os tópicos que você mais
-        gosta. Queremos ajudá-lo a tomar decisões informadas e a descobrir novas oportunidades de aprendizado e
-        crescimento pessoal. Acreditamos que o diálogo é fundamental para criar uma comunidade vibrante e enriquecedora.
-        Nosso objetivo é tornar sua visita ao nosso blog uma experiência gratificante, onde você possa encontrar
-        informações relevantes, se sentir inspirado e conectar-se com pessoas de mentalidade semelhante. Esperamos que
-        você se sinta em casa e que este espaço seja um refúgio virtual para explorar, aprender e crescer. Agradecemos
-        por fazer parte da nossa comunidade e esperamos que você desfrute de tudo o que nosso blog tem a oferecer.
-        Prepare-se para uma jornada empolgante, repleta de descobertas e inspiração! Equipe do Blog
+        Bem-vindo ao nosso blog! Aqui no nosso espaço virtual, você encontrará
+        um universo de informações, inspirações e histórias fascinantes. Nosso
+        blog é um lugar onde compartilhamos ideias, conhecimentos e experiências
+        sobre uma ampla variedade de temas. Navegue por nossas páginas e
+        descubra artigos e postagens emocionantes sobre tecnologia. Queremos ser
+        uma fonte confiável e enriquecedora de conteúdo para todos os nossos
+        leitores. Queremos cativar sua imaginação, instigar sua curiosidade e
+        oferecer perspectivas únicas sobre os tópicos que você mais gosta.
+        Queremos ajudá-lo a tomar decisões informadas e a descobrir novas
+        oportunidades de aprendizado e crescimento pessoal. Acreditamos que o
+        diálogo é fundamental para criar uma comunidade vibrante e
+        enriquecedora. Nosso objetivo é tornar sua visita ao nosso blog uma
+        experiência gratificante, onde você possa encontrar informações
+        relevantes, se sentir inspirado e conectar-se com pessoas de mentalidade
+        semelhante. Esperamos que você se sinta em casa e que este espaço seja
+        um refúgio virtual para explorar, aprender e crescer. Agradecemos por
+        fazer parte da nossa comunidade e esperamos que você desfrute de tudo o
+        que nosso blog tem a oferecer. Prepare-se para uma jornada empolgante,
+        repleta de descobertas e inspiração! Equipe do Blog
       </p>
     </div>
   );
--- src/pages/ItemPage/ItemPage.jsx
+++ src/pages/ItemPage/ItemPage.jsx
@@ -7,9 +7,12 @@ const ItemPage = () => {
   const item = itens.find((el) => Number(el.id) === Number(param.id));
   return (
     <div className={styles.itemPage}>
-      <h1>{item.descricao}</h1>
-      <span>{item.autor}</span>
-      <p>{item.texto}</p>
+      <img src="/logo192.png" alt="React Logo" />
+      <div>
+        <h1>{item.descricao}</h1>
+        <span>{item.autor}</span>
+        <p>{item.texto}</p>
+      </div>
     </div>
   );
 };
